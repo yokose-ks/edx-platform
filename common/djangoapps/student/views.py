765399eabc724741f588e4b47100b19905310d3c
Joe Blaylock <jrbl@jrbl.org>
2013-10-18 11:08:10 -0700

    Certificates: URL endpoint for cert gen
    * API endpoint for certificate generation, an authenticated  post with course
      id requests that grading be carried out and a cert generated for
      request.user in that course, using the usual grading and certificate
      machinery (ie, it does not imply whitelisting, though whitelists and
      blacklists will be respected)
      - Logs each request as it comes in
      - Calls xq.add_cert() and consequently, does grading synchronously on
        this app host and then queues request for certificate agent.
      - example usage:
        ```
        curl --data "student_id=9999&course_id=Stanford/2013/Some_Class" http://127.0.0.1:8000/request_certificate
        ```
    
    * Studio advanced setting added, "certificates_show_before_end", which
      determines whether a course should permit certificates to be downloadable
      by students before the coures's end date has passed.
      - Modifications to dashboard view and templates to allow display of
        certificate download links before course has ended.
        (XXX: may declare failing students as failing before the course has ended.)
      - To test, turn the setting on in a course which hasn't ended yet, and
        force certificate generation for a student, then check their
        dashboard.
    
    * TODO:
      - make grading calls into a celery task so they happen asynchronously
      - needs tests
      - needs pep8/pylint
    
    * TODO:
      - needs tests
      - needs pep8/pylint

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -33,7 +33,6 @@ from django_future.csrf import ensure_csrf_cookie
 from django.utils.http import cookie_date, base36_to_int
 from django.utils.translation import ugettext as _
 from django.views.decorators.http import require_POST, require_GET
-from django.contrib.admin.views.decorators import staff_member_required
 
 from ratelimitbackend.exceptions import RateLimitException
 
@@ -178,7 +177,7 @@ def cert_info(user, course):
     'survey_url': url, only if show_survey_button is True
     'grade': if status is not 'processing'
     """
-    if not course.has_ended():
+    if not course.may_certify():
         return {}
 
     return _cert_info(user, course, certificate_status_for_student(user, course.id))
@@ -270,6 +269,15 @@ def _cert_info(user, course, cert_status):
     """
     Implements the logic for cert_info -- split out for testing.
     """
+    # simplify the status for the template using this lookup table
+    template_state = {
+        CertificateStatuses.generating:   'generating',
+        CertificateStatuses.regenerating: 'generating',
+        CertificateStatuses.downloadable: 'ready',
+        CertificateStatuses.notpassing:   'notpassing',
+        CertificateStatuses.restricted:   'restricted',
+    }
+
     default_status = 'processing'
 
     default_info = {'status': default_status,
@@ -280,15 +288,6 @@ def _cert_info(user, course, cert_status):
 
     if cert_status is None:
         return default_info
-
-    # simplify the status for the template using this lookup table
-    template_state = {
-        CertificateStatuses.generating: 'generating',
-        CertificateStatuses.regenerating: 'generating',
-        CertificateStatuses.downloadable: 'ready',
-        CertificateStatuses.notpassing: 'notpassing',
-        CertificateStatuses.restricted: 'restricted',
-    }
 
     status = template_state.get(cert_status['status'], default_status)
 
