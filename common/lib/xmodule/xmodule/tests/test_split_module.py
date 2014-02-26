5dfb0315ee1cd13f6046b9d43ad0b4ebac22efc2
Diana Huang <dkh@edx.org>
2014-02-26 12:06:59 -0500

    Update xml format to handle relative locations
    New sample XML:
    <vertical url_name="split_test_vert">
        <split_test url_name="split1" user_partition_id="0" group_id_to_child='{"0": "html/split_test_cond0", "1": "html/split_test_cond1"}'>
           <html url_name="split_test_cond0">condition 0</html>
           <html url_name="split_test_cond1">condition 1</html>
        </split_test>
    </vertical>

--- common/lib/xmodule/xmodule/tests/test_split_module.py
+++ common/lib/xmodule/xmodule/tests/test_split_module.py
@@ -21,7 +21,7 @@ class SplitTestModuleTest(TestCase):
         self.split_test_module_descriptor = SplitTestDescriptor()
 
         test_xml = """
-            <split_test url_name="split1" user_partition_id="0" group_id_to_child='{"0": "i4x://MITx/6.00x/html/split_test_cond0", "1": "i4x://MITx/6.00x/html/split_test_cond1"}'>
+            <split_test url_name="split1" user_partition_id="0" group_id_to_child='{"0": "html/split_test_cond0", "1": "html/split_test_cond1"}'>
                 <html url_name="split_test_cond0">condition 0</html>
                 <html url_name="split_test_cond1">condition 1</html>
             </split_test>
