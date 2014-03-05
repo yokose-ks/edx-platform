b1d554a17b443e215d4882045e26fa564eea8e40
root <root@ip-172-31-12-122.ap-northeast-1.compute.internal>
2014-03-05 21:51:44 +0900

    testtesttetetetete

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Usage: manage.py {lms|cms} [--settings env] ...
+hoge-Usage: manage.py {lms|cms} [--settings env] ...
 
 Run django management commands. Because edx-platform contains multiple django projects,
 the first argument specifies which project to run (cms [Studio] or lms [Learning Management System]).
