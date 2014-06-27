314aca93f8fa4d325890fa68b2b518cae9f00727
Stephen Sanchez <steve@edx.org>
2014-06-27 11:28:11 -0400

    Updating settings and requirements for lms and cms to support file upload

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -599,6 +599,7 @@ OPTIONAL_APPS = (
     'submissions',
     'openassessment',
     'openassessment.assessment',
+    'openassessment.fileupload',
     'openassessment.workflow',
     'openassessment.xblock'
 )
